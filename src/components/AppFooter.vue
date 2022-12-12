c29e0f877a206441096cd8fb6ab7a217464005d0
DeEchteZeeuw <rubendalebout@gmail.com>
2022-12-12 13:58:23 +0100

    added footer route

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,16 +5,22 @@ import { build, search, alertCircle } from 'ionicons/icons';
     <footer class="container-fluid bg-primary position-fixed bottom-0">
         <div class="row p-2">
             <div class="col d-flex flex-column justify-content-center text-center">
-                <ion-icon class="fs-1 m-auto" id="footer-build-icon" :icon="build"></ion-icon>
-                <span>Active task</span>
+                <router-link to="/" class="d-flex flex-column align-items-center justify-content-center">
+                    <ion-icon class="fs-1 m-auto" id="footer-build-icon" :icon="build"></ion-icon>
+                    <span>Active task</span>
+                </router-link>
             </div>
             <div class="col d-flex flex-column justify-content-center text-center">
-                <ion-icon class="fs-1 m-auto" id="footer-search-icon" :icon="search"></ion-icon>
-                <span>Search</span>
+                <router-link to="/search" class="d-flex flex-column align-items-center justify-content-center">
+                    <ion-icon class="fs-1 m-auto" id="footer-search-icon" :icon="search"></ion-icon>
+                    <span>Search</span>
+                </router-link>
             </div>
             <div class="col d-flex flex-column justify-content-center text-center">
-                <ion-icon class="fs-1 m-auto" id="footer-alertCircle-icon" :icon="alertCircle"></ion-icon>
-                <span>Help</span>
+                <router-link to="/help" class="d-flex flex-column align-items-center justify-content-center">
+                    <ion-icon class="fs-1 m-auto" id="footer-alertCircle-icon" :icon="alertCircle"></ion-icon>
+                    <span>Help</span>
+                </router-link>
             </div>
         </div>
     </footer>
