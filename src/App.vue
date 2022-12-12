6aea6f2ae4f4837bc74fc1248ca596915f109c68
DeEchteZeeuw <rubendalebout@gmail.com>
2022-12-12 11:43:59 +0100

    Design replicated in basic

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,65 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 
-import { triangle, ellipse, square  } from 'ionicons/icons';
+import { triangle, ellipse, square, notifications, settings, bookmark, checkmarkCircle, grid, build, search, alertCircle } from 'ionicons/icons';
 </script>
 
 <template>
-  <header class="bg-dark">
-    <div class="bg-primary p-2 d-flex justify-content-end gap-2">
-      <ion-icon id="iconbar-square" :icon="square"></ion-icon>
-      <ion-icon id="iconbar-ellipse" :icon="ellipse"></ion-icon>
-      <ion-icon id="iconbar-triangle" :icon="triangle"></ion-icon>
-    </div>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header class="bg-dark text-primary container-fluid">
+        <div class="row">
+            <div class="col bg-primary p-2 d-flex justify-content-end gap-2">
+                <ion-icon id="iconbar-square" :icon="square"></ion-icon>
+                <ion-icon id="iconbar-ellipse" :icon="ellipse"></ion-icon>
+                <ion-icon id="iconbar-triangle" :icon="triangle"></ion-icon>
+            </div>
+        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="row text-light p-3">
+            <div class="col">
+                <img alt="Vue logo" class="logo" src="@/assets/realestatecare/logo/logo.svg" />
+            </div>
+            <div class="col d-flex justify-content-end gap-2 align-items-center">
+                <ion-icon class="fs-1" id="notifications-icon" :icon="notifications"></ion-icon>
+                <ion-icon class="fs-1" id="notifications-icon" :icon="settings"></ion-icon>
+            </div>
+        </div>
+    </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <main class="container-fluid">
+        <div class="row">
+            <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
+                <ion-icon class="fs-1 m-auto" id="navigation-bookmark-icon" :icon="bookmark"></ion-icon>
+                <span>Scheduled</span>
+            </div>
+            <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
+                <ion-icon class="fs-1 m-auto" id="navigation-checkmarkCircle-icon" :icon="checkmarkCircle"></ion-icon>
+                <span>Completed</span>
+            </div>
+            <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
+                <ion-icon class="fs-1 m-auto" id="navigation-grid-icon" :icon="grid"></ion-icon>
+                <span>Knowledge base</span>
+            </div>
+            <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
+                <ion-icon class="fs-1 m-auto" id="navigation-settings-icon" :icon="settings"></ion-icon>
+                <span>Settings</span>
+            </div>
+        </div>
+    </main>
 
-  <RouterView />
+    <footer class="container-fluid bg-primary">
+        <div class="row p-2">
+            <div class="col d-flex flex-column justify-content-center text-center">
+                <ion-icon class="fs-1 m-auto" id="footer-build-icon" :icon="build"></ion-icon>
+                <span>Active task</span>
+            </div>
+            <div class="col d-flex flex-column justify-content-center text-center">
+                <ion-icon class="fs-1 m-auto" id="footer-search-icon" :icon="search"></ion-icon>
+                <span></span>
+            </div>
+            <div class="col d-flex flex-column justify-content-center text-center">
+                <ion-icon class="fs-1 m-auto" id="footer-alertCircle-icon" :icon="alertCircle"></ion-icon>
+                <span></span>
+            </div>
+        </div>
+    </footer>
 </template>
