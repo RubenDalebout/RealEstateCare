d3faa87b83316a36822f0c209283a60bb17c97d7
DeEchteZeeuw <rubendalebout@gmail.com>
2022-12-15 13:55:21 +0100

    outlined the code

--- src/components/Inspections.vue
+++ src/components/Inspections.vue
@@ -1,52 +1,51 @@
 <script>
-import axios from 'axios'; // Import axios
-import InspectionDetails from '@/components/InspectionDetails.vue';
+    import axios from 'axios'; // Import axios
+    import InspectionDetails from '@/components/InspectionDetails.vue';
 
-export default {
-  components: {
-    InspectionDetails,
-  },
-  data() {
-    return {
-      showModal: false,
-      selectedInspection: {},
-      data: [],
+    export default {
+        components: {
+            InspectionDetails,
+        },
+        data() {
+            return {
+            showModal: false,
+            selectedInspection: {},
+            data: [],
+            };
+        },
+        mounted() {
+            axios.get('src/data/inspections.json')
+            .then((response) => {
+                this.data = response.data;
+            });
+        },
+        methods: {
+            showInspectionDetails(inspection) {
+            this.showModal = true;
+            this.selectedInspection = inspection;
+            },
+        },
     };
-  },
-  mounted() {
-    axios.get('src/data/inspections.json') // Use the correct URL for the file you want to load
-      .then((response) => {
-        this.data = response.data;
-      });
-  },
-  methods: {
-    showInspectionDetails(inspection) {
-      this.showModal = true;
-      this.selectedInspection = inspection;
-    },
-  },
-};
 </script>
 
 <template>
-    <!-- Add the v-if directive to show or hide the component -->
     <inspection-details
         v-if="showModal"
         @close="showModal = false"
         :inspection="selectedInspection"
     />
     <div>
-      <div v-for="(inspection, index) in data.inspections" class="border-top border-bottom" :key="index">
-        <h2>Inspection {{ index + 1 }}</h2>
-        <div>
-          <h3>Details:</h3>
-          <p>Inspector: {{ inspection.details.inspector }}</p>
-          <p>Date: {{ inspection.details.date }}</p>
-          <p>Finished: {{ inspection.details.finished }}</p>
-          <button type="button" class="btn btn-primary" @click="showInspectionDetails(inspection)">
-            View Details
-          </button>
+        <div v-for="(inspection, index) in data.inspections" class="border-top border-bottom" :key="index">
+            <h2>Inspection {{ index + 1 }}</h2>
+            <div>
+                <h3>Details:</h3>
+                <p>Inspector: {{ inspection.details.inspector }}</p>
+                <p>Date: {{ inspection.details.date }}</p>
+                <p>Finished: {{ inspection.details.finished }}</p>
+                <button type="button" class="btn btn-primary" @click="showInspectionDetails(inspection)">
+                View Details
+                </button>
+            </div>
         </div>
-      </div>
     </div>
-  </template>  
+</template>  
