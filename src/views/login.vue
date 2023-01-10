c99915759226b34dce9853bbe80af5fa83bfc989
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-10 23:30:50 +0100

    designed login form and added users.json

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,3 +1,23 @@
 <template>
-    login template
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-sm"></div>
+            <div class="col-sm">
+                <form action="" id="login" name="login" method="post">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="johndoe@realestatecare.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;">
+                    </div>
+                    <div>
+                        <input type="submit" value="Login" class="btn btn-primary">
+                    </div>
+                </form>
+            </div>
+            <div class="col-sm"></div>
+        </div>
+    </div>
 </template>
