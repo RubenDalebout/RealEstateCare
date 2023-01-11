2b678ff8e67a3b40b573dd6fc690316fb67432b9
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-11 02:04:52 +0100

    created an knowledge base

--- src/views/knowledge-base.vue
+++ src/views/knowledge-base.vue
@@ -1,3 +1,45 @@
 <template>
-    Knowledgebase page
+    <main class="container">
+        <div class="row">
+            <div class="col-sm-6 mt-3">
+                <div class="border rounded p-3">
+                    <h5>Example document 1</h5>
+                    <a @click="downloadFile('src/files/example-document-1.docx')" download class="btn btn-primary">Download</a>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-3">
+                <div class="border rounded p-3">
+                    <h5>Example document 2</h5>
+                    <a @click="downloadFile('src/files/example-document-2.docx')" download class="btn btn-primary">Download</a>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-3">
+                <div class="border rounded p-3">
+                    <h5>Example document 3</h5>
+                    <a @click="downloadFile('src/files/example-document-3.docx')" download class="btn btn-primary">Download</a>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-3">
+                <div class="border rounded p-3">
+                    <h5>Example document 4</h5>
+                    <a @click="downloadFile('src/files/example-document-4.docx')" download class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
+
+<script>
+import FileSaver from 'file-saver';
+export default {
+  methods: {
+    downloadFile(fileUrl) {
+    fetch(fileUrl)
+        .then(response => response.blob())
+        .then(blob => {
+        FileSaver.saveAs(blob, fileUrl.split('/').pop());
+        });
+    },
+  },
+}
+</script>
