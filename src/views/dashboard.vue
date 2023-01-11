25da52e199b267ec90b3ddddefe247d42d7ff54c
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-11 01:32:58 +0100

    updated settings field

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -7,25 +7,25 @@ import { settings, bookmarkOutline, checkmarkCircle, grid, build, search, alertC
             <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
                 <router-link to="/scheduled" class="d-flex flex-column align-items-center justify-content-center">
                     <ion-icon class="m-auto" id="navigation-bookmarkOutline-icon" :icon="bookmarkOutline"></ion-icon>
-                    <span class="fw-bold fs-5 mt-4">Scheduled</span>
+                    <span class="fw-bold fs-6 mt-4">Scheduled</span>
                 </router-link>
             </div>
             <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
                 <router-link to="/completed" class="d-flex flex-column align-items-center justify-content-center">
                     <ion-icon class="m-auto" id="navigation-checkmarkCircle-icon" :icon="checkmarkCircle"></ion-icon>
-                    <span class="fw-bold fs-5 mt-4">Completed</span>
+                    <span class="fw-bold fs-6 mt-4">Completed</span>
                 </router-link>
             </div>
             <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
                 <router-link to="/knowledge-base" class="d-flex flex-column align-items-center justify-content-center">
                     <ion-icon class="m-auto" id="navigation-grid-icon" :icon="grid"></ion-icon>
-                    <span class="fw-bold fs-5 mt-4">Knowledge base</span>
+                    <span class="fw-bold fs-6 mt-4">Knowledge base</span>
                 </router-link>
             </div>
             <div class="col-6 d-flex flex-column align-items-center justify-content-center p-3">
                 <router-link to="/settings" class="d-flex flex-column align-items-center justify-content-center">
                     <ion-icon class="m-auto" id="navigation-settings-icon" :icon="settings"></ion-icon>
-                    <span class="fw-bold fs-5 mt-4">Settings</span>
+                    <span class="fw-bold fs-6 mt-4">Settings</span>
                 </router-link>
             </div>
         </div>
--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1,3 +1,52 @@
 <template>
-    Settingspage
+    <main class="container">
+        <div class="row">
+            <div class="col-2-sm"></div>
+            <div class="col-sm">
+                <h1>Profile settings</h1>
+                <form @submit.prevent="" id="accountdetails" name="accountdetails" method="post">
+                    <div class="mb-3">
+                        <label for="firstname" class="form-label">Firstname</label>
+                        <input type="firstname" class="form-control" id="firstname" name="firstname" placeholder="John">
+                    </div>
+                    <div class="mb-3">
+                        <label for="lastname" class="form-label">Lastname</label>
+                        <input type="lastname" class="form-control" id="lastname" name="lastname" placeholder="Do">
+                    </div>
+                    <div class="mb-3">
+                        <label for="newpassword" class="form-label">New password</label>
+                        <input type="newpassword" class="form-control" id="newpassword" name="newpassword" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;">
+                    </div>
+                    <div class="mb-3">
+                        <label for="newpassword" class="form-label">Confirm new password</label>
+                        <input type="newpassword" class="form-control" id="newpassword" name="newpassword" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;">
+                    </div>
+                    <div>
+                        <input type="submit" value="Change account" class="btn btn-primary">
+                    </div>
+                </form>
+            </div>
+            <div class="col-2-sm"></div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-2-sm"></div>
+            <div class="col-sm">
+                <h1>Application settings</h1>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked">
+                    <label class="form-check-label" for="flexSwitchCheckChecked">Dark modus</label>
+                </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked">
+                    <label class="form-check-label" for="flexSwitchCheckChecked">Notifications</label>
+                </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked">
+                    <label class="form-check-label" for="flexSwitchCheckChecked">Sounds</label>
+                </div>
+            </div>
+            <div class="col-2-sm"></div>
+        </div>
+    </main>
 </template>
