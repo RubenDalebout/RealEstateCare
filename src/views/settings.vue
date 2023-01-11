6e1ec0a699f1dfa3ea7048c993ff5f953cf171ab
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-11 15:12:53 +0100

    changes

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -34,19 +34,55 @@
             <div class="col-sm">
                 <h1>Application settings</h1>
                 <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="darkModeSwitch" name="darkModeSwitch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="darkModeSwitch" v-model="settings.darkTheme">
                     <label class="form-check-label" for="darkModeSwitch">Dark modus</label>
                 </div>
                 <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="notificationsSwitch" name="notificationsSwitch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="notificationsSwitch" v-model="settings.notifications">
                     <label class="form-check-label" for="notificationsSwitch">Notifications</label>
                 </div>
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="soundSwitch" name="soundSwitch">
+                <div class="form-check form-switch mb-3">
+                    <input class="form-check-input" type="checkbox" role="switch" id="soundSwitch" v-model="settings.sounds">
                     <label class="form-check-label" for="soundSwitch">Sounds</label>
                 </div>
+                <button class="btn btn-primary" @click="updateSettings()">Save changes</button>
             </div>
             <div class="col-2-sm"></div>
         </div>
     </main>
 </template>
+
+<script>
+import fs from 'fs'
+
+export default {
+    data() {
+        // Get user from local storage
+        const user = JSON.parse(localStorage.getItem('user'));
+        // return user settings
+        return {
+            settings: user.settings,
+            id: user.id,
+        };
+    },
+    methods: {
+        updateSettings() {
+            // Get all users from the JSON file
+            const users = JSON.parse(fs.readFileSync('src/data/users.json'));
+
+            // Find the index of the user by id
+            const userIndex = users.findIndex(user => user.id === this.id);
+            if (userIndex !== -1) {
+                // update the settings of user
+                users[userIndex].settings = this.settings;
+
+                // Write the updated users array to the JSON file
+                fs.writeFileSync('src/data/users.json', JSON.stringify(users));
+                console.log("Successfully updated settings in JSON file");
+            } else {
+                console.log("User not found");
+            }
+        },
+    },
+};
+</script>
