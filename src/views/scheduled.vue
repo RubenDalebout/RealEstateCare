d1e0af1d0a34965af72f81018b549a0839d3792d
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-13 19:27:55 +0100

    new scheduled page with other api and json

--- src/views/scheduled.vue
+++ src/views/scheduled.vue
@@ -1,18 +1,42 @@
 <template>
-    <inspections :finished="showFinished"/>
+    <div>
+        <h1>Address List</h1>
+        <div class="list-group">
+            <a href="#" v-for="address in addresses" :key="address.id" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                {{ address.street }}, {{ address.city }}
+                <button class="btn btn-primary" @click="viewInspections(address.id)">View Inspections</button>
+            </a>
+        </div>
+    </div>
 </template>
-
+  
 <script>
-import Inspections from '@/components/Inspections.vue'
-
-export default {
-    components: {
-        Inspections
-    },
-    data() {
-        return {
-            showFinished: false,
+    import axios from 'axios';
+  
+    export default {
+        name: "AddressList",
+        data() {
+            return {
+                addresses: []
         };
     },
-}
+    created() {
+        axios({
+            method: 'get',
+            url: `https://jsonbin.org/63c1a09815ab31599e35cf00`,
+            headers: {
+                'X-Master-Key': '$2b$10$6OQ5plkCt1vMLN8m7VMniOP5RSMQB3WOfPoQlYh/JNbs2xeF7psUu'
+            }
+        }).then(response => {
+            this.addresses = response.data.addresses;
+        }).catch(error => {
+            console.log(error);
+        });
+    },
+    methods: {
+        viewInspections(id) {
+            this.$router.push({ name: 'inspections', params: { id } });
+        }
+    }
+};
 </script>
