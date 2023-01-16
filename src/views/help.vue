a039278399900f8cbddc614c3363245f089094b5
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-16 14:36:30 +0100

    Added content to help view

--- src/views/help.vue
+++ src/views/help.vue
@@ -1,3 +1,41 @@
 <template>
-    Helppage
+    <main class="container">
+        <h2 class="my-3">Getting Started</h2>
+        <ul class="list-group">
+            <li class="list-group-item">Download the RealEstateCare app from the App Store or Google Play.</li>
+            <li class="list-group-item">Log in with your provided username and password.</li>
+            <li class="list-group-item">Once logged in, you will be directed to the main dashboard.</li>
+            <li class="list-group-item">From here, you can access your scheduled inspections, completed inspections, the knowledge base and settings.</li>
+        </ul>
+
+        <h2 class="my-3">Scheduled Inspections</h2>
+        <ul class="list-group">
+            <li class="list-group-item">From the main dashboard, click on "Scheduled Inspections" to view a list of all the addresses that have been assigned to you.</li>
+            <li class="list-group-item">Click on an address to view the list of inspections assigned to that address.</li>
+            <li class="list-group-item">Click on an inspection to open it and begin filling in the information.</li>
+            <li class="list-group-item">You can take photos, add notes and complete multiple sections within an inspection.</li>
+            <li class="list-group-item">Once you have completed an inspection, you can submit it for review by your supervisor.</li>
+        </ul>
+
+        <h2 class="my-3">Completed Inspections</h2>
+        <ul class="list-group">
+            <li class="list-group-item">From the main dashboard, click on "Completed Inspections" to view a list of all the addresses that you have completed inspections for.</li>
+            <li class="list-group-item">Click on an address to view the list of inspections completed for that address.</li>
+            <li class="list-group-item">Click on an inspection to view it, make edits or add additional information.</li>
+        </ul>
+
+        <h2 class="my-3">Knowledge Base</h2>
+        <ul class="list-group">
+            <li class="list-group-item">From the main dashboard, click on "Knowledge Base" to access a variety of helpful documents and resources.</li>
+            <li class="list-group-item">Here you can find information on how to use the app, best practices for completing inspections, and other useful information.</li>
+        </ul>
+
+        <h2 class="my-3">Settings</h2>
+        <ul class="list-group">
+            <li class="list-group-item">From the main dashboard, click on "Settings" to access your profile information, change your password, and customize your notification preferences.</li>
+            <li class="list-group-item">In the profile section, you can update your contact information and personal details.</li>
+            <li class="list-group-item">In the password section, you can change your password for added security.</li>
+            <li class="list-group-item">In the notifications section, you can turn on or off notifications for new inspections and completed inspections.</li>
+        </ul>
+    </main>
 </template>
