ff11ff0363ad8e4e4ebe4b7062ccd9d7d0ea80ed
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-16 20:45:35 +0100

    do calls that didnt fire

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,34 @@ export default {
     AppHeader,
     AppFooter,
     NoWifiModal
+  },
+  mounted() {
+    window.addEventListener('online', this.handleConnectionChange);
+  },
+  methods: {
+    handleConnectionChange() {
+      let axiosCalls = JSON.parse(localStorage.getItem('axiosCalls')) || [];
+      let executed = 0;
+      let notExecuted = 0;
+
+      if (axiosCalls.length > 0) {
+        axiosCalls.forEach(call => {
+          axios[call.method](call.url, call.data)
+            .then(response => {
+              executed++;
+              call.executed = true;
+            })
+            .catch(error => {
+              notExecuted++;
+            });
+        });
+
+        axiosCalls = axiosCalls.filter(call => !call.executed);
+        localStorage.setItem('axiosCalls', JSON.stringify(axiosCalls));
+        console.log(`Executed ${executed} calls.`);
+        console.log(`Not executed ${notExecuted} calls.`);
+      }
+    },
   }
 }
 </script>
