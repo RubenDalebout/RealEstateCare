b51eb57fa2ac4bdd048d4c77db4890ee3793f263
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-17 00:46:16 +0100

    Added comments to main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,31 @@
+// Import Bootstrap CSS and JS
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/js/bootstrap.min.js'
 import "bootstrap"
 
+// Import Ionic CSS
 import "@ionic/core/css/ionic.bundle.css"
 
+// Import Ionic Vue, IonIcon and IonToast
 import { IonicVue, IonIcon, IonToast } from '@ionic/vue';
 
+// Import createApp function from Vue
 import { createApp } from 'vue'
+
+// Import App.vue and router
 import App from './App.vue'
 import router from './router'
 
+// Check if the user has dark theme enabled
 if (localStorage.getItem('user')) {
     if (JSON.parse(localStorage.getItem('user'))) {
         const usr = JSON.parse(localStorage.getItem('user'));
 
+        // Check if the user has settings and if the dark theme is enabled
         if (usr.settings != undefined && usr.settings != null) {
             if (usr.settings.darkTheme != undefined && usr.settings.darkTheme != null) {
                 if (usr.settings.darkTheme) {
+                    // Enable dark theme for elements with the attribute "darkTheme"
                     document.querySelectorAll('[darkTheme]').forEach(e => {
                         if (e.getAttribute('disabled') === 'true') {
                             e.removeAttribute('disabled');
@@ -30,8 +39,11 @@ if (localStorage.getItem('user')) {
     }
 }
 
+// Create the Vue app
 const app = createApp(App);
 
+// Use Ionic Vue, router, IonIcon and IonToast
 app.use(IonicVue).use(router).use(IonIcon).component('ion-icon', IonIcon, {silent: true}).use(IonToast);
 
+// Mount the app to the element with id "app"
 app.mount('#app');
