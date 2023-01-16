4ade2750e66992d80252aa1931c672bd58af27fe
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-16 14:47:09 +0100

    added wifi check component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
     <app-header />
     <router-view></router-view>
     <app-footer />
+    <NoWifiModal />
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
 import AppFooter from '@/components/AppFooter.vue'
+import NoWifiModal from '@/components/NoWifiModal.vue'
+
 
 export default {
   components: {
     AppHeader,
-    AppFooter
+    AppFooter,
+    NoWifiModal
   }
 }
 </script>
