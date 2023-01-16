24f79030dea3eaa646ecd0e1117bdcd1e261c2c5
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-16 14:45:29 +0100

    Created wificheck component

---
+++ src/components/WifiCheck.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-if="!navigator.onLine" class="text-center">
+      <b-modal ref="noWifiModal" :visible="true" title="Oops!">
+        <p class="my-4">You don't have wifi. All your actions will be run when there is wifi again</p>
+        <b-btn variant="primary" @click="closeModal">Close</b-btn>
+      </b-modal>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    closeModal() {
+      this.$refs.noWifiModal.hide();
+    }
+  },
+  mounted() {
+    window.addEventListener("online", this.closeModal);
+  },
+  beforeDestroy() {
+    window.removeEventListener("online", this.closeModal);
+  }
+};
+</script>
