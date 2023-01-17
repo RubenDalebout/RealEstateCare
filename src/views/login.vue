4b0eb77a523f8d1a5873a0c4b8f6ce56c60a32ef
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-17 13:13:08 +0100

    installed vuex

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,6 +3,7 @@
         <div class="row mt-5">
             <div class="col-sm"></div>
             <div class="col-sm">
+                <!-- form to handle the login -->
                 <form @submit.prevent="loginRoute" id="login" name="login" method="post">
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
@@ -20,6 +21,7 @@
             <div class="col-sm"></div>
         </div>
 
+        <!-- toast message container -->
         <div v-if="showToast" class="toast-container">
             <div v-bind:class="{'show': showToast}" :class="'toast toast-' + toastType">
                 {{ toastMessage }}
@@ -52,6 +54,7 @@ export default {
                     }
                 });
 
+                // If response is not success show a error toast
                 if (response.status !== 200) {
                     this.toastType = 'error';
                     this.toastMessage = 'Could not verify the user';
@@ -69,6 +72,7 @@ export default {
                 // Filter the data using your condition
                 const user = response.data.record.filter(user => user.email === submitEvent.target.elements.email.value && user.password === submitEvent.target.elements.password.value);
 
+                // Check if there is a user found
                 if (user.length > 0) {
                     // Removing the password key from the user object
                     delete user[0].password;
@@ -79,6 +83,7 @@ export default {
                     // navigate user to home page
                     this.$router.go('/'); 
                 } else {
+                    // show an error toast if there is no user found
                     this.toastType = 'error';
                     this.toastMessage = 'There is no user with those credentials found';
                     this.showToast = true;
@@ -90,6 +95,7 @@ export default {
                     }, 3000);
                 }
             } catch (err) {
+                // something went wrong show an error toast
                 this.toastType = 'error';
                 this.toastMessage = (err.code != 'ERR_NETWORK') ? 'There has been an error occurred, contact the developer!' : 'You dont have wifi!';
                 this.showToast = true;
